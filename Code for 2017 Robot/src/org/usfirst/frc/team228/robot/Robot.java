cd7384bd20b4afc9c436fa79031fd2084459fd65
JJNCreator <jakebayerct@gmail.com>
2017-02-18 10:56:44 -0500

    Changed variables
    Shooter motors are now TalonSRX variables

--- Code for 2017 Robot/src/org/usfirst/frc/team228/robot/Robot.java
+++ Code for 2017 Robot/src/org/usfirst/frc/team228/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DigitalInput;
 import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.TalonSRX;
 
 
 /**
@@ -60,7 +61,7 @@ public class Robot extends IterativeRobot {
 	Encoder leftDriveEncoder, rightDriveEncoder;
 	
 	//Talons
-	CANTalon shooterMotor1,shooterMotor2,shooterMotor3;
+	TalonSRX shooterMotor1,shooterMotor2,shooterMotor3;
 	
 	//drive function
 	RobotDrive drivetrain;
